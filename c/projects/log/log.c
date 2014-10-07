b4f2538677f1a50f26e4aaf87e7dd9115dc78c7c
Adam Dodd <a-dodd@outlook.com>
2014-10-07 19:27:25 +0100

    Updated logging library!

--- c/projects/log/log.c
+++ c/projects/log/log.c
@@ -2,34 +2,24 @@
 #define _POSIX_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
 #include <fcntl.h>
-//#include <pthread.h>
-
-//#define MAIN_LOG_SRC
+#include <linux/limits.h>
 #include "log.h"
 
-#define LOG_MAX_LEN 1000
-// FIXME temporary!!
+#define LOG_MAX_LEN PIPE_BUF
 
-#define PATH_MAX 4096
-
-int flags;
-char logFileStr[PATH_MAX];
-FILE* logFile;
-char logEntry[LOG_MAX_LEN];
-struct timeval timeAtStart;
-//pthread_mutex_t logMutex = PTHREAD_MUTEX_INITIALIZER;
-
-struct timeval relTime()
+struct timeval relTime(struct timeval timeAtStart)
 {
-	struct timeval timeNow;
 	struct timeval toReturn;
+	struct timeval timeNow;
 	
 	gettimeofday(&timeNow, NULL);
 	
@@ -38,32 +28,39 @@ struct timeval relTime()
 	return toReturn;
 }
 
-int logInit(const char* fileName, const int req_flags)
+int logInit(log_t* handle, const char* fileName, const int reqLogLevels,
+				const int reqFlags, const logType_t type, int* threadID)
 {
-	gettimeofday(&timeAtStart, NULL);
+	handle->logFileStr = malloc(sizeof(char) * PATH_MAX);
 	
-	logFileStr[0] = 0;
+	if (handle->logFileStr == NULL)
+		return -1;
+	
+	gettimeofday(&handle->timeAtStart, NULL);
+	
+	handle->logFileStr[0] = 0;
 	
 	if ((fileName != NULL) && (fileName[0] != 0))
 	{
-		if ((logFile = fopen(fileName, "w")) == NULL)
+		if ((handle->logFile = fopen(fileName, "w")) == NULL)
 		{
 			perror("fopen");
 			return -1;
 		}
 		
-		strcpy(logFileStr, fileName);
+		strcpy(handle->logFileStr, fileName);
 	}
 	else
-		logFile = stderr;
+		handle->logFile = stderr;
 	
-	setlinebuf(logFile); // Essential!
+	setlinebuf(handle->logFile); /* Essential! */
 	
-	flags = req_flags;
+	handle->logLevels = reqLogLevels;
+	handle->flags = reqFlags;
 	
-	if (LOG_IS_FLAG(LOG_FLAG_CLOEXEC))
+	if (LOG_PTR_IS_FLAG(LOG_FLAG_CLOEXEC))
 	{
-		int logFD = fileno(logFile);
+		int logFD = fileno(handle->logFile);
 		
 		if (logFD == -1)
 		{
@@ -78,56 +75,204 @@ int logInit(const char* fileName, const int req_flags)
 		}
 	}
 	
+	handle->type = type;
+	handle->threadID = threadID;
+	
+	if ((type != single) && (threadID == NULL))
+	{
+		fprintf(stderr, "threadID cannot be null when multi-threaded!\n");
+		EXITF;
+	}
+	
+	char typeStr[16];
+	
+	if (type == single)
+		strcpy(typeStr, "single");
+	else if (type == pthread)
+		strcpy(typeStr, "pthread");
+	else if (type == mpi)
+		strcpy(typeStr, "mpi");
+	else
+		strcpy(typeStr, "unknown");
+	
+	logPrint(*handle, LOG_ARGS, LOG_INFO, "Log library started (v%d.%d.%d)",
+				MAJOR_VER, MINOR_VER, BUILD_VER);
+	
+	if (threadID != NULL)
+		logPrint(*handle, LOG_ARGS, LOG_INFO, "threadID = %d, type = %s",
+					*threadID, typeStr);
+	else
+		logPrint(*handle, LOG_ARGS, LOG_INFO, "type = %s", typeStr);
+	
+	logPrint(*handle, LOG_ARGS, LOG_INFO, "loglevels mask = 0x%.08x",
+				reqLogLevels);
+	
 	return 0;
 }
 
-int logPrint2(const int logLevel, const char* str, const char* file,
-				  const int line)
+int logPrint(log_t handle, const char* file, const int line,
+				  const int logLevel, const char* fmt, ...)
 {
-	int res;
-	char logLevelStr[8];
-	
-	/*
-	if ((res = pthread_mutex_lock(&logMutex)) != 0)
+	if (LOG_IS_LL(logLevel))
 	{
-		fprintf(stderr, "pthread_mutex_lock returned %d\n", res);
-		return -1;
+		va_list ap;
+		char logLevelStr[8];
+		char logEntry[LOG_MAX_LEN];
+		
+		switch (logLevel)
+		{
+			case LOG_INFO:
+				strcpy(logLevelStr, "INFO");
+				break;
+			case LOG_WARN:
+				strcpy(logLevelStr, "WARN");
+				break;
+			case LOG_ERR:
+				strcpy(logLevelStr, "ERR");
+				break;
+			case LOG_BUG:
+				strcpy(logLevelStr, "BUG");
+				break;
+			case LOG_TRACE_NET:
+				strcpy(logLevelStr, "NET*");
+				break;
+			case LOG_TRACE_DB:
+				strcpy(logLevelStr, "DB*");
+				break;
+			default:
+				strcpy(logLevelStr, "UNKN");
+				break;
+		}
+		
+		va_start(ap, fmt);
+		vsnprintf(logEntry, LOG_MAX_LEN, fmt, ap);
+		va_end(ap);
+		
+		struct timeval tv = relTime(handle.timeAtStart);
+		
+		if (LOG_IS_FLAG(LOG_FLAG_SRC_INFO))
+		{
+			if (handle.type == single)
+				fprintf(handle.logFile, "[%5d.%.06d] <%s:%d> %s: %s\n",
+						  tv.tv_sec, tv.tv_usec, file, line, logLevelStr, logEntry);
+			else
+				fprintf(handle.logFile, "[%5d.%.06d] <%s:%d> #%d %s: %s\n",
+						  tv.tv_sec, tv.tv_usec, file, line, *handle.threadID,
+						  logLevelStr, logEntry);
+		}
+		else
+		{
+			if (handle.type == single)
+				fprintf(handle.logFile, "[%5d.%.06d] %s: %s\n",
+						  tv.tv_sec, tv.tv_usec, logLevelStr, logEntry);
+			else
+				fprintf(handle.logFile, "[%5d.%.06d] #%d %s: %s\n",
+						  tv.tv_sec, tv.tv_usec, *handle.threadID, logLevelStr,
+						  logEntry);
+		}
 	}
-	*/
 	
-	if (logLevel == 0)
-		strcpy(logLevelStr, "INFO:");
-	else if (logLevel == 1)
-		strcpy(logLevelStr, "WARN:");
-	else
-		strcpy(logLevelStr, "ERR:");
-	
-	struct timeval tv = relTime();
+	return 0;
+}
+
+int logHexdump(log_t handle, const char* file, const int line, const char* str,
+					const int len)
+{
+	int i, j;
+	struct timeval tv = relTime(handle.timeAtStart);
 	
 	if (LOG_IS_FLAG(LOG_FLAG_SRC_INFO))
-		fprintf(logFile, "[%5d.%.06d] <%s:%d> %s %s\n",
-				  tv.tv_sec, tv.tv_usec, file, line, logLevelStr, str);
-	else
-		fprintf(logFile, "[%5d.%.06d] %s %s\n",
-				  tv.tv_sec, tv.tv_usec, logLevelStr, str);
-	
-	/*
-	if ((res = pthread_mutex_unlock(&logMutex)) != 0)
 	{
-		fprintf(stderr, "pthread_mutex_unlock returned %d\n", res);
-		return -1;
+		if (handle.type == single)
+			fprintf(handle.logFile, "[%5d.%.06d] <%s:%d> HEX:\n", tv.tv_sec,
+					  tv.tv_usec, file, line);
+		else
+			fprintf(handle.logFile, "[%5d.%.06d] <%s:%d> #%d HEX:\n", tv.tv_sec,
+					  tv.tv_usec, file, line, *handle.threadID);
 	}
-	*/
+	else
+	{
+		if (handle.type == single)
+			fprintf(handle.logFile, "[%5d.%.06d] HEX:\n", tv.tv_sec, tv.tv_usec);
+		else
+			fprintf(handle.logFile, "[%5d.%.06d] #%d HEX:\n", tv.tv_sec,
+					  tv.tv_usec, *handle.threadID);
+	}
 	
+	fprintf(handle.logFile, "---BEGIN_HEX---");
+	
+	for (i = 0; i < len; i++)
+	{
+		if ((i % 16) == 0)
+			fprintf(handle.logFile, "\n%.08x  ", i);
+		
+		fprintf(handle.logFile, "%.02hhx ", str[i]);
+		
+		if (i > 0)
+		{
+			if ((i % 8) == 7)
+				fputc(' ', handle.logFile);
+		
+			if ((i % 16) == 15)
+			{
+				/* Use isprint() to print the chars, then print offset after new
+					line */
+				
+				fputc('|', handle.logFile);
+				
+				for (j = i - 15; j <= i; j++)
+					if (isprint(str[j]))
+						fputc(str[j], handle.logFile);
+					else
+						fputc('.', handle.logFile);
+				
+				fputc('|', handle.logFile);
+			}
+		}
+	}
+	
+	/* What if the hexdump didn't finish perfectly at (i % 16) == 0? */
+	
+	if ((i % 16) != 0)
+	{
+		/* Calculate the number of spaces to add to the ASCII area */
+		int spaces = (16 - (i % 16)) * 3 + 1;
+		
+		if ((i % 16) < 8)
+			spaces++;
+			/* Don't forget the space in the middle! */
+		
+		for (j = 0; j < spaces; j++)
+			fputc(' ', handle.logFile);
+		
+		fputc('|', handle.logFile);
+		
+		for (j = i - (i % 16); j < i; j++)
+			if (isprint(str[j]))
+				fputc(str[j], handle.logFile);
+			else
+				fputc('.', handle.logFile);
+		
+		fputc('|', handle.logFile);
+	}
+	
+	fprintf(handle.logFile, "\n%.08x\n---END_HEX---\n", i);
 	return 0;
 }
 
-int logCleanup()
+int logHexdumpz(log_t handle, const char* file, const int line, const char* str)
 {
-	logPrint(LOG_INFO, "Logging stopped");
-		
-	if (logFileStr[0] != 0)
-		if ((fclose(logFile)) != 0)
+	return logHexdump(handle, file, line, str, strlen(str));
+}
+
+int logCleanup(log_t handle)
+{
+	logPrint(handle, LOG_ARGS, LOG_INFO, "Logging stopped");
+	
+	free(handle.logFileStr);
+	
+	if (handle.logFileStr[0] != 0)
+		if ((fclose(handle.logFile)) != 0)
 		{
 			perror("fclose");
 			return -1;
--- c/projects/log/log.h
+++ c/projects/log/log.h
@@ -1,24 +1,73 @@
 #ifndef LOG_H
 #define LOG_H
 
-#define LOG_MAX_LEN 1000
+#define _BSD_SOURCE
+#include <sys/time.h>
 
-#define LOG_INFO	0
-#define LOG_WARN	1
-#define LOG_ERR	2
+#define LOG_INFO					0x0000
+#define LOG_WARN					0x0001
+#define LOG_ERR					0x0002
+#define LOG_BUG					0x0004
+#define LOG_TRACE_NET			0x0010
+#define LOG_TRACE_DB				0x0020
+/*#define LOG_TRACE_METHODS		0x1000 */
+/* Surely this is too anal? Use a debugger to get this info! */
+#define LOG_ALL					0xffff
 
 #define LOG_FLAG_NONE		0
 #define LOG_FLAG_CLOEXEC	1
 #define LOG_FLAG_SRC_INFO	2 /* Log also prints out file and line at call! */
 
-#define LOG_IS_FLAG(x) ((flags & x) == x)
+#define LOG_PTR_IS_LL(x)	((handle->logLevels & x) == x)
+#define LOG_IS_LL(x)			((handle.logLevels & x) == x)
 
-extern char logEntry[];
+#define LOG_PTR_IS_FLAG(x)	((handle->flags & x) == x)
+#define LOG_IS_FLAG(x)		((handle.flags & x) == x)
 
-#define logPrint(x,y)		logPrint2(x, y, __FILE__, __LINE__)
-extern int logPrint2(const int, const char*, const char*, const int);
+#define LOG_ARGS				__FILE__, __LINE__
 
-extern int logInit(const char*, const int);
+#define EXITF				exit(EXIT_FAILURE)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum
+{
+	single,
+	pthread,
+	mpi
+} logType_t;
+
+typedef struct
+{
+	char* logFileStr;
+	FILE* logFile;
+	struct timeval timeAtStart;
+	logType_t type;
+	int* threadID;
+	int logLevels;
+	int flags;
+} log_t;
+
+/*#define logPrint(x, y, z)			logPrint2(x, __FILE__, __LINE__, y, z)
+extern int logPrint2(log_t, const char*, const int, const int, char*);
+#define logPrint(x,y,z,...)	logPrint2v(x, __FILE__, __LINE__, y, z, __VA_ARGS__) */
+
+/* XXX
+	Note: variadic macros don't work in conjunction with variadic functions!
+*/
+
+extern int logPrint(log_t, const char*, const int, const int, const char*, ...);
+
+extern int logInit(log_t*, const char*, const int, const int, const logType_t,
+						 int*);
+extern int logHexdump(log_t, const char*, const int, const char*, const int);
+extern int logHexdumpz(log_t, const char*, const int, const char*);
 extern int logCleanup();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
