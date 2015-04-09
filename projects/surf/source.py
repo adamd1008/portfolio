c97ca20c24119274da43349c18644923c0b24a07
Adam Dodd <a-dodd@outlook.com>
2015-04-09 19:54:38 +0100

    Added surf project!

---
+++ projects/surf/source.py
@@ -0,0 +1,153 @@
+import socket
+import struct
+import string
+import time
+import sys
+import sqlite3
+import webbrowser
+
+class SourceServer(object):
+	"""Base class for Valve Source engine game servers"""
+	
+	def __init__(self, nick, addr, port):
+		self.nick = nick
+		self.addr = addr
+		self.port = port
+		
+		# Internal variables to determine status
+		self._pinged = False
+		self._online = bool()
+		
+		# Variables from ping
+		self._header = int()
+		self._protocol = int()
+		self._name = str()
+		self._map = str()
+		self._folder = str()
+		self._game = str()
+		self._gameID = int()
+		self._players = int()
+		self._max_players = int()
+		self._bots = int()
+		self._type = str()
+		self._env = str()
+		self._vis = bool()
+		self._vac = bool()
+	
+	def _ping(self, timeout):
+		request_packet = "\xff\xff\xff\xff\x54\x53\x6f\x75\x72\x63\x65\x20\x45" \
+							  "\x6e\x67\x69\x6e\x65\x20\x51\x75\x65\x72\x79\x00"
+		
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.settimeout(timeout)
+		sock.connect((self.addr, self.port))
+		sock.sendall(request_packet)
+		
+		try:
+			reply = sock.recv(4096)
+		except sock.error as e:
+			reply = None
+		finally:
+			sock.close()
+		
+		return reply
+	
+	def ping(self, timeout = 0.35, maxAttempts = 2):
+		attempts = 0
+		
+		while attempts < maxAttempts:
+			reply = self._ping(timeout)
+			
+			if not reply == None:
+				break
+			
+			attempts += 1
+		
+		if reply == None:
+			self._online = False
+		else:
+			self._online = True
+			
+			self._header = struct.unpack('B', reply[4])[0]
+			self._protocol = int(struct.unpack('B', reply[5])[0])
+			
+			i = 6
+			
+			while reply[i] != '\x00':
+				i += 1
+			
+			self._name = reply[6:i]
+			i += 1
+			j = i
+			
+			while reply[i] != '\x00':
+				i += 1
+			
+			self._map = reply[j:i].lower()
+			i += 1
+			j = i
+			
+			while reply[i] != '\x00':
+				i += 1
+			
+			self._folder = reply[j:i]
+			i += 1
+			j = i
+			
+			while reply[i] != '\x00':
+				i += 1
+			
+			self._game = reply[j:i]
+			i += 1
+			self._gameID = struct.unpack('H', reply[i:i + 2])[0]
+			i += 2
+			self._players = int(struct.unpack('B', reply[i])[0])
+			i += 1
+			self._max_players = int(struct.unpack('B', reply[i])[0])
+			i += 1
+			self._bots = int(struct.unpack('B', reply[i])[0])
+			i += 1
+			self._type = reply[i]
+			i += 1
+			self._env = reply[i]
+			i += 1
+			self._vis = bool(struct.unpack('B', reply[i])[0])
+			i += 1
+			self._vac = bool(struct.unpack('B', reply[i])[0])
+		
+		self._pinged = True
+	
+	def join(self, pingInterval = 0.2):
+		self.ping()
+		
+		if self._online and self._players >= self._max_players:
+			sys.stdout.write("Server \"%s\" is full (%d/%d)\nListening for free " \
+								  + "space" % (self.nick, self._players,
+								  self._max_players))
+			
+			while self._online and self._players >= self._max_players:
+				self.ping()
+				sys.stdout.write(".")
+				time.sleep(pingInterval)
+			
+			sys.stdout.write("\n")
+			
+			if self._online:
+				print "Space now available (%d/%d)" % (self._players,
+						self._max_players)
+				webbrowser.open("steam://connect/%s:%d" % (self.addr, self.port))
+			else:
+				print "Server \"%s\" has gone offline" % (self.nick,)
+		else:
+			print "Joining server (%d/%d)" % (self._players, self._max_players)
+			webbrowser.open("steam://connect/%s:%d" % (self.addr, self.port))
+			# Consider FancyURLopener() if this doesn't work on some platform?
+	
+	@staticmethod
+	def pingAll(servers):
+		for server in servers:
+			server.ping()
+	
+	@staticmethod
+	def join(servers, sID):
+		servers[sID].join()
---
+++ projects/surf/surf.py
@@ -0,0 +1,232 @@
+import sqlite3
+from source import SourceServer
+
+class SurfMap(object):
+	"""Class for Valve GoldSrc/Source surf maps"""
+	
+	def __init__(self, name, tier = -1, rating = -1, stages = -1, bonus = -1,
+					 complete = False, favourite = False):
+		self._name = name.lower()
+		self.tier = tier
+		self.rating = rating
+		self.stages = stages
+		self.bonus = bonus
+		self.complete = bool(complete)
+		self.favourite = bool(favourite)
+	
+	def get(self):
+		"""Look up map by name"""
+		
+		conn = sqlite3.connect("surf.db")
+		conn.isolation_level = None
+		cur = conn.cursor()
+		cur.execute("SELECT `tier`, `rating`, `stages`, `bonus`, " \
+						"`complete`, `favourite` FROM maps WHERE `name`=?",
+						(self._name,))
+		
+		row = cur.fetchone()
+		
+		if row == None:
+			ret = False
+		else:
+			self.tier = int(row[0])
+			self.rating = int(row[1])
+			self.stages = int(row[2])
+			self.bonus = int(row[3])
+			self.complete = bool(row[4])
+			self.favourite = bool(row[5])
+			ret = True
+		
+		cur.close()
+		conn.close()
+		
+		return ret
+	
+	def insert(self):
+		"""Insert map by name"""
+		
+		conn = sqlite3.connect("surf.db")
+		conn.isolation_level = None
+		cur = conn.cursor()
+		cur.execute("INSERT INTO maps (`name`, `tier`, `rating`, `stages`, " \
+						"`bonus`, `complete`, `favourite`) VALUES " \
+						"(?, ?, ?, ?, ?, ?, ?, ?)", (self._name, self.tier,
+						self.rating, self.stages, self.bonus, self.complete,
+						self.favourite))
+		
+		cur.close()
+		conn.close()
+	
+	def update(self):
+		"""Update map by name"""
+		
+		conn = sqlite3.connect("surf.db")
+		conn.isolation_level = None
+		cur = conn.cursor()
+		
+		cur.execute("UPDATE maps SET `tier`=?, `rating`=?, `stages`=?, " \
+						"`bonus`=?, `complete`=?, `favourite`=? WHERE `name`=?",
+						(self.tier, self.rating, self.stages, self.bonus,
+						self.complete, self.favourite, self._name))
+		
+		cur.close()
+		conn.close()
+
+class SurfDB(object):
+	"""Static database class for surf servers and maps"""
+	
+	@staticmethod
+	def getServers():
+		conn = sqlite3.connect("surf.db")
+		conn.isolation_level = None
+		cur = conn.cursor()
+		cur.execute("SELECT `name`, `address`, `port` FROM servers")
+		
+		ret = list()
+		
+		for row in cur:
+			ret.append(SourceServer(row[0], row[1], row[2]))
+		
+		cur.close()
+		conn.close()
+		
+		return ret
+	
+	@staticmethod
+	def getMaps():
+		conn = sqlite3.connect("surf.db")
+		conn.isolation_level = None
+		cur = conn.cursor()
+		cur.execute("SELECT `name`, `tier`, `rating`, `stages`, `bonus`, " \
+						"`complete`, `favourite` FROM maps")
+		
+		ret = list()
+		
+		for row in cur:
+			ret.append(SurfMap(row[0], row[1], row[2], row[3], row[4], row[5],
+						  row[6]))
+		
+		cur.close()
+		conn.close()
+		
+		return ret
+	
+	@staticmethod
+	def prettyPrint():
+		maps = SurfDB.getMaps()
+		servers = SurfDB.getServers()
+		SourceServer.pingAll(servers)
+		
+		first = True
+		maxIDLen = 0
+		maxNickLen = 0
+		maxMapLen = 10
+		
+		for i, server in enumerate(servers):
+			nickLen = len(server.nick)
+			
+			if nickLen > maxNickLen:
+				maxNickLen = nickLen
+			
+			idLen = len(str(i))
+			
+			if idLen > maxIDLen:
+				maxIDLen = idLen
+			
+			if server._online:
+				mapLen = len(server._map)
+				
+				if mapLen > maxMapLen:
+					maxMapLen = mapLen
+		
+		print "=" * (maxNickLen + 7 + maxMapLen + 7) \
+				+ "====================================="
+		print "ID | Server" + " " * (maxNickLen + 1) + "| Game | Map" + " " * \
+				(maxMapLen + 4) + "| Tier | Rating | Comp"
+		
+		for i, server in enumerate(servers):
+			if first:
+				print "===|" + "=" * (maxNickLen + 8) + "|======|" + "=" \
+						* (maxMapLen + 8) + "|======|========|======"
+				first = False
+			else:
+				print "---|" + "-" * (maxNickLen + 8) + "|------|" + "-" \
+						* (maxMapLen + 8) + "|------|--------|------"
+		
+			if server._online:
+				# Find map the server is on!!
+			
+				thisMap = None
+			
+				for map_ in maps:
+					if map_._name == server._map:
+						thisMap = map_
+						break
+			
+				if thisMap == None:
+					outStr = "{:>2d} | {:<" + str(maxNickLen) + "s} {:>2d}/{:<2d} " \
+								"| {:>4d} | {:<" + str(maxMapLen) + "s}  -/-  |    - " \
+								"|      - |   -  "
+				
+					print outStr.format(i, server.nick, server._players,
+											  server._max_players, server._gameID,
+											  server._map)
+				else:
+					outStr = "{:>2d} | {:<" + str(maxNickLen) + "s} {:>2d}/{:<2d} " \
+								"| {:>4d} | {:<" + str(maxMapLen) + "s} {:>2d}/{:<2d}" \
+								" | {:>4d} | {:>6d} | {:s}"
+				
+					print outStr.format(i, server.nick, server._players,
+											  server._max_players, server._gameID,
+											  server._map, thisMap.stages, thisMap.bonus,
+											  thisMap.tier, thisMap.rating,
+											  str(thisMap.complete))
+			else:
+				outStr = "{:>2d} | {:<" + str(maxNickLen + 6) + "s} |      | {:<" \
+						+ str(maxMapLen + 6) + "s} |      |        |"
+			
+				print outStr.format(i, server.nick, "(offline!)")
+	
+		print "=" * (maxNickLen + 7 + maxMapLen + 7) \
+				+ "====================================="
+	
+	@staticmethod
+	def pp():
+		SurfDB.prettyPrint()
+	
+	@staticmethod
+	def monitor(delay = 180):
+		while True:
+			SurfDB.prettyPrint()
+			time.sleep(delay)
+	
+	@staticmethod
+	def mon(delay = 180):
+		SurfDB.monitor()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
